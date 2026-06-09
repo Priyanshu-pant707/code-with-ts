b02db5821fea510b018c471c4514c471c0f181e8
PRIYANSHU_PANT <pantpriyanshu707@gmail.com>
2026-06-09 16:36:02 +0000

    uu

---
+++ 02_object/object.ts
@@ -0,0 +1,27 @@
+type car = {
+    name:string;
+    price:number;
+    color:string;
+    isNew:boolean;
+}
+
+
+const car1:car={
+    name:"BMW",
+    price:5000000,
+    color:"black",
+    isNew:true
+}
+
+
+const car2:car={
+    name:"Audi",
+    price:4000000,
+    color:"white",
+    isNew:false
+}
+
+
+
+
+ 
